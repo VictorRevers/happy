09049350f277d30787110d765d013333034061c6
VictorRevers <victorrkasnowski@gmail.com>
2020-10-18 00:21:44 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Routes from './routes';
+
+import 'leaflet/dist/leaflet.css';
+import './styles/global.css';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
